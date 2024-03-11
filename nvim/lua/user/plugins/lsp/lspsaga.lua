59ac2d9b1db16eda628bf095aed928896aba5d71
plmoer <plmoer@gmail.com>
2024-03-11 13:45:18 -0400

    Update lspsaga.lua to compatible with the new node (v20.11.1)

--- nvim/lua/user/plugins/lsp/lspsaga.lua
+++ nvim/lua/user/plugins/lsp/lspsaga.lua
@@ -4,7 +4,20 @@ if not saga_status then
 	return
 end
 
-saga.init_lsp_saga({
+-- saga.init_lsp_saga({
+--	-- keybinds for navigation in lspsaga window
+--	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+--	-- use enter to open file with finder
+--	finder_action_keys = {
+--		open = "<CR>",
+--	},
+--	-- use enter to open file with definition preview
+--	definition_action_keys = {
+--		edit = "<CR>",
+--	},
+-- })
+
+saga.setup({
 	-- keybinds for navigation in lspsaga window
 	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
 	-- use enter to open file with finder
@@ -16,3 +29,4 @@ saga.init_lsp_saga({
 		edit = "<CR>",
 	},
 })
+
