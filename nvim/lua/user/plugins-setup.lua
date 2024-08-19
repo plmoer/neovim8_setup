ab7bb5cfbd1cc9a97aa05d1471c0209097ffd907
plmoer <plmoer@gmail.com>
2024-08-18 21:23:14 -0400

    update telescope-fzf-native setting in Packer

--- nvim/lua/user/plugins-setup.lua
+++ nvim/lua/user/plugins-setup.lua
@@ -59,7 +59,11 @@ return packer.startup(function(use)
 	use("nvim-lualine/lualine.nvim")
 
 	-- fuzzy finding w/ telescope
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	-- use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+	})
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
 	-- autocompletion
