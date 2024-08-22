5c6e805ea2c1b00922b6e20059734523a1b3d67a
plmoer <plmoer@gmail.com>
2024-08-21 21:51:54 -0400

    added formatting for c using Conform

--- nvim/lua/user/plugins/conform.lua
+++ nvim/lua/user/plugins/conform.lua
@@ -1,26 +1,31 @@
 require("conform").setup({
 	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
 		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black", "autopep8"},
-    ["*"] = { "codespell" },
-    -- clang = {"clang--format"},
-		-- You can customize some of the format options for the filetype (:help conform.format)
-		rust = { "rustfmt", lsp_format = "fallback" },
-		-- Conform will run the first available formatter
-		javascript = { "prettierd", "prettier", stop_after_first = true },
+		nix = { "nixfmt" },
+		python = { "autopep8" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
 		timeout_ms = 500,
 		lsp_format = "fallback",
 	},
-  vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*",
+		callback = function(args)
+			require("conform").format({ bufnr = args.buf })
+		end,
+	}),
 	-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 	-- 	conform.format({
 	-- 		lsp_fallback = true,
