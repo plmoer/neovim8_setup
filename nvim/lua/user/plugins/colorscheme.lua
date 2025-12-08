a24a15f4b15f374b553cb8a2009b3cc979d22fc2
plmoer <plmoer@gmail.com>
2025-12-08 15:46:00 -0500

    Switch colorscheme from Gruvbox to Tokyonight

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,13 +1,21 @@
+-- return {
+--   "morhetz/gruvbox",  -- Gruvbox colorscheme plugin
+--   priority = 1000,
+--   config = function()
+--     vim.o.background = "dark"  -- or "light" for a lighter version
+--     vim.cmd([[hi SpellBad cterm=underline ctermfg=203 guifg=#ff5f5f]])
+--     vim.cmd([[hi SpellLocal cterm=underline ctermfg=203 guifg=#ff5f5f]])
+--     vim.cmd([[hi SpellRare cterm=underline ctermfg=203 guifg=#ff5f5f]])
+--     vim.cmd([[hi SpellCap cterm=underline ctermfg=203 guifg=#ff5f5f]])
+
+--     vim.cmd("colorscheme gruvbox")  -- Apply the colorscheme
+--   end,
+-- }
 return {
-  "morhetz/gruvbox",  -- Gruvbox colorscheme plugin
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
   config = function()
-    vim.o.background = "dark"  -- or "light" for a lighter version
-    vim.cmd([[hi SpellBad cterm=underline ctermfg=203 guifg=#ff5f5f]])
-    vim.cmd([[hi SpellLocal cterm=underline ctermfg=203 guifg=#ff5f5f]])
-    vim.cmd([[hi SpellRare cterm=underline ctermfg=203 guifg=#ff5f5f]])
-    vim.cmd([[hi SpellCap cterm=underline ctermfg=203 guifg=#ff5f5f]])
-
-    vim.cmd("colorscheme gruvbox")  -- Apply the colorscheme
+    vim.cmd("colorscheme tokyonight")
   end,
 }
